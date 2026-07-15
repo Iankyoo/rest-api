35a15b69bc1c004666e6ff5b4bc2ebfcb6eda2b8
Iankyoo <contato.iankyoo@gmail.com>
2026-07-14 21:18:08 -0300

    feat: Meu primeiro teste unitário em Java

---
+++ src/test/java/com/restaurant/rest_api/Service/CategoryServiceTest.java
@@ -0,0 +1,48 @@
+package com.restaurant.rest_api.Service;
+
+import com.restaurant.rest_api.dto.CategoryRequest;
+import com.restaurant.rest_api.dto.CategoryResponse;
+import com.restaurant.rest_api.entity.Category;
+import com.restaurant.rest_api.repository.CategoryRepository;
+import com.restaurant.rest_api.service.CategoryService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @Test
+    public void createCategoryTest(){
+        CategoryRequest categoryRequest = new CategoryRequest("Drinks", "");
+        Category savedCategory = Category.builder()
+                .id(1L)
+                .name("Drinks")
+                .description("")
+                .build();
+
+
+        when(categoryRepository.save(any(Category.class)))
+                .thenReturn(savedCategory);
+
+        CategoryResponse response = categoryService.createCategory(categoryRequest);
+
+        assertEquals(1L,response.id());
+        assertEquals("Drinks", response.name());
+        assertEquals("", response.description());
+        verify(categoryRepository).save(any(Category.class));
+    }
+}
